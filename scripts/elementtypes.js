37140f6efc99824860a74c55a185f282849c52df
Olexandr Panov <futupas@gmail.com>
2018-05-27 23:36:37 +0300

    add name in the top of table, Scale(x, y, z), Skew(x, y)

--- scripts/elementtypes.js
+++ scripts/elementtypes.js
@@ -1,14 +1,69 @@
 var elementtypes = [
     {
         type: 'sketch',
-        properties: ['perspective', 'backgroundColor'],
+        properties: ['perspective', 'background'],
         styles: [],
         
         draw: function (o) {
             var prw = document.getElementById('preview');
             prw.style.perspective = o.properties['perspective'] + 'px';
-            prw.style.backgroundColor = o.properties['backgroundColor'];
+            prw.style.backgroundColor = o.properties['background'];
             return prw;
+        },
+        edit: function(o) {
+            var table = document.createElement('table');
+
+            var tr = document.createElement('tr');
+            var td1 = document.createElement('td');
+            td1.innerText = 'Name';
+            var td2 = document.createElement('td');
+            var b = document.createElement('b');
+            b.innerText = o.name;
+            td2.appendChild(b);
+            tr.appendChild(td1); tr.appendChild(td2);
+            table.appendChild(tr);
+            
+            var drawfunc = this.draw
+            var createTr = function (caption, propname, from, to, step, isText, length) {
+                var tr = document.createElement('tr');
+                var td1 = document.createElement('td');
+                var td2 = document.createElement('td');
+                td1.innerText = caption;
+                var input = document.createElement('input');
+                if (isText === true) {
+                    input.setAttribute('type', 'text');
+                    input.setAttribute('maxlength', length);
+                } else {
+                    input.setAttribute('type', 'number');
+                    input.setAttribute('min', from);
+                    input.setAttribute('max', to);
+                    input.setAttribute('step', step);
+                }
+                
+                input.value = o.properties[propname];
+                input.propname = propname;
+                input.elem = o;
+                input.oninput = function (e) {
+                    o.properties[this.propname] = this.value * 1;
+                    var propelement = this.elem;
+                    Elements.filter(function(el){return el.name == propelement.name})[0].properties[this.propname] = 
+                            this.value;
+                    
+                    var div = document.getElementById('preview');
+                    var divstyles = div.style;
+                    divstyles.perspective = o.properties['perspective'] + 'px';
+                    divstyles.background = o.properties['background'];
+                }
+                td2.appendChild(input);
+                tr.appendChild(td1);
+                tr.appendChild(td2);
+                table.appendChild(tr);
+            }
+            createTr('Perspective', 'perspective', 0, 10000, 100, false, 0);
+            createTr('Background', 'background', 0, 0, 0, true, 50);
+
+            document.getElementById('element').innerHTML = '';
+            document.getElementById('element').appendChild(table);
         }
     }, 
     {
@@ -20,6 +75,11 @@ var elementtypes = [
             'rotateX',
             'rotateY',
             'rotateZ',
+            'scaleX',
+            'scaleY',
+            'scaleZ',
+            'skewX',
+            'skewY',
             'width',
             'height',
             'background',
@@ -37,7 +97,12 @@ var elementtypes = [
                     'px) translateZ(' + o.properties['z'] +
                     'px) rotateX(' + o.properties['rotateX'] +
                     'deg) rotateY(' + o.properties['rotateY'] +
-                    'deg) rotateZ(' + o.properties['rotateZ'] + 'deg)';
+                    'deg) rotateZ(' + o.properties['rotateZ'] + 
+                    'deg) scaleX(' + o.properties['scaleX'] + 
+                    ') scaleY(' + o.properties['scaleY'] + 
+                    ') scaleZ(' + o.properties['scaleZ'] + 
+                    ') skewX(' + o.properties['skewX'] + 
+                    'deg) skewY(' + o.properties['skewY'] + 'deg)';
             divstyles.width = o.properties['width'] + 'px';
             divstyles.height = o.properties['height'] + 'px';
             divstyles.background = o.properties['background'];
@@ -47,6 +112,17 @@ var elementtypes = [
         },
         edit: function(o) {
             var table = document.createElement('table');
+
+            var tr = document.createElement('tr');
+            var td1 = document.createElement('td');
+            td1.innerText = 'Name';
+            var td2 = document.createElement('td');
+            var b = document.createElement('b');
+            b.innerText = o.name;
+            td2.appendChild(b);
+            tr.appendChild(td1); tr.appendChild(td2);
+            table.appendChild(tr);
+
             var drawfunc = this.draw
             var createTr = function (caption, propname, from, to, step, isText, length) {
                 var tr = document.createElement('tr');
@@ -75,14 +151,17 @@ var elementtypes = [
                     
                     var div = document.querySelector('#preview div[data-elname="'+propelement.name+'"]');
                     var divstyles = div.style;
-                    divstyles.padding = '0px';
-                    divstyles.margin = '0px';
                     divstyles.transform = 'translateX(' + o.properties['x'] +
                             'px) translateY(' + o.properties['y'] +
                             'px) translateZ(' + o.properties['z'] +
                             'px) rotateX(' + o.properties['rotateX'] +
                             'deg) rotateY(' + o.properties['rotateY'] +
-                            'deg) rotateZ(' + o.properties['rotateZ'] + 'deg)';
+                            'deg) rotateZ(' + o.properties['rotateZ'] + 
+                            'deg) scaleX(' + o.properties['scaleX'] + 
+                            ') scaleY(' + o.properties['scaleY'] + 
+                            ') scaleZ(' + o.properties['scaleZ'] + 
+                            ') skewX(' + o.properties['skewX'] + 
+                            'deg) skewY(' + o.properties['skewY'] + 'deg)';
                     divstyles.width = o.properties['width'] + 'px';
                     divstyles.height = o.properties['height'] + 'px';
                     divstyles.background = o.properties['background'];
@@ -99,6 +178,11 @@ var elementtypes = [
             createTr('Rotate X', 'rotateX', -360, 360, 5, false, 0);
             createTr('Rotate Y', 'rotateY', -360, 360, 5, false, 0);
             createTr('Rotate Z', 'rotateZ', -360, 360, 5, false, 0);
+            createTr('Scale X', 'scaleX', -10, 10, 0.5, false, 0);
+            createTr('Scale Y', 'scaleY', -10, 10, 0.5, false, 0);
+            createTr('Scale Z', 'scaleZ', -10, 10, 0.5, false, 0);
+            createTr('Skew X', 'skewX', -360, 360, 5, false, 0);
+            createTr('Skew Y', 'skewY', -360, 360, 5, false, 0);
             createTr('Width', 'width', 0, 1000, 10, false, 0);
             createTr('Height', 'height', 0, 1000, 10, false, 0);
             createTr('Background', 'background', 0, 0, 0, true, 50);
